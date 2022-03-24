8663d5bd5696a55b4d53fe6ce7c374ac0a8f8beb
jetpacula <melnikov.jr@gmail.com>
2022-03-24 18:21:21 +0000

    server script now prints client config

--- server.sh
+++ server.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
+if [ "$#" -ne 3 ]; then
+	echo "usage confname (wg1) subnet (1) port 2222"
+	exit
+fi
+
 apt update
 apt install wireguard
 
 cd /etc/wireguard
 umask 077
+
+IPADDR=$(curl -s ipinfo.io/ip)
 
 CONFIG_NAME=$1
 SUBNET=$2
@@ -24,7 +31,7 @@ echo $CLIENT_PUBKEY > ./client_public.key
 echo $CLIENT_PRIVKEY > ./client_private.key
 
 echo "[Interface]" > ./$CONFIG_NAME.conf
-echo "Address = 10.253.$SUBNET.1/24" >> ./$CONFIG_NAME.conf
+echo "Address = 10.20.$SUBNET.1/24" >> ./$CONFIG_NAME.conf
 echo "" >> ./$CONFIG_NAME.conf
 echo "SaveConfig = true" >> ./$CONFIG_NAME.conf
 echo "PrivateKey = $SERVER_PRIVKEY" >> ./$CONFIG_NAME.conf
@@ -35,8 +42,19 @@ echo "PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i 
 echo "" >> ./$CONFIG_NAME.conf
 echo "[Peer]" >> ./$CONFIG_NAME.conf
 echo "PublicKey = $CLIENT_PUBKEY" >> ./$CONFIG_NAME.conf
-echo "AllowedIPs = 10.253.$SUBNET.2/32" >> ./$CONFIG_NAME.conf
+echo "AllowedIPs = 10.20.$SUBNET.2/32" >> ./$CONFIG_NAME.conf
 
+echo "[Interface]" 
+echo "Address = 10.20.$SUBNET.2/32"
+echo "PrivateKey = $CLIENT_PRIVKEY"
+echo "DNS = 1.1.1.1"
+echo ""
+echo "[Peer]"
+echo "PublicKey = $SERVER_PUBKEY"
+echo "Endpoint = $IPADDR:$PORT"
+echo "AllowedIPs = 0.0.0.0/0, ::/0"
+
+sed -i -e 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 
 systemctl enable wg-quick@$CONFIG_NAME
 chown -R root:root /etc/wireguard/
